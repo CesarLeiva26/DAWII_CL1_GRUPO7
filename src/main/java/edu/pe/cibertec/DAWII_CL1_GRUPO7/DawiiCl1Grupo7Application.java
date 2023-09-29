7262489cbd94d6f4eb45db53bee03b26fdff1949
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-09-28 20:00:34 -0500

    Primer Commit - Cesar Leiva

---
+++ src/main/java/edu/pe/cibertec/DAWII_CL1_GRUPO7/DawiiCl1Grupo7Application.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.DAWII_CL1_GRUPO7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1Grupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1Grupo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/DAWII_CL1_GRUPO7/DawiiCl1Grupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.DAWII_CL1_GRUPO7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl1Grupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
